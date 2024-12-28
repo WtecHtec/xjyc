9f51d64d77b8e4beedc086a038223a3c31af7180
wtechtec <504105925@qq.com>
2024-12-28 22:03:06 +0800

    还原路由

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -1,9 +1,9 @@
 export * from './projects'
-// export * from './friends'
-// export * from './changelog'
-// export * from './education'
-// export * from './career'
-// export * from './activity'
+export * from './friends'
+export * from './changelog'
+export * from './education'
+export * from './career'
+export * from './activity'
 
 
 // personal info
