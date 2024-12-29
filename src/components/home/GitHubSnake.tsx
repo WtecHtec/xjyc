982caf177773b4f10e5ad23b43ed54cac37ce9eb
wtechtec <504105925@qq.com>
2024-12-29 08:47:23 +0800

    优化

--- src/components/home/GitHubSnake.tsx
+++ src/components/home/GitHubSnake.tsx
@@ -5,12 +5,12 @@ export default function GitHubSnake() {
     <div className="w-full overflow-hidden">
       <div className='dark:hidden'>
         <img 
-          src="https://github.com/WtecHtec/WtecHtec/blob/output/github-contribution-grid-snake.svg" 
+          src="/github-contribution-snake/github-contribution-grid-snake.svg" 
           alt="github-contribution" />
       </div>
       <div className='hidden dark:block'>
         <img 
-          src="https://github.com/WtecHtec/WtecHtec/blob/output/github-contribution-grid-snake-dark.svg" 
+          src="/github-contribution-snake/github-contribution-grid-snake-dark.svg" 
           alt="github-contribution" />
       </div>
     </div>
